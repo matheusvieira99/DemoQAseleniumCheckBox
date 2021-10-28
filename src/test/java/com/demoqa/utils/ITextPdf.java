9791218e7ae24b11bbc8c4119e924451cd9b299f
Matheus Vieira <matheus-vieira12@hotmail.com>
2021-10-28 14:21:07 -0300

    fix: pdf generator

--- src/test/java/com/demoqa/utils/ITextPdf.java
+++ src/test/java/com/demoqa/utils/ITextPdf.java
@@ -11,7 +11,7 @@ public class ITextPdf {
 	
 	public void iTextPdf() throws Exception {
 		
-		String file = "C:\\Users\\Matheus\\Desktop\\projects\\DemoQA\\target\\pdf.pdf";
+		String file = "C:\\Users\\Matheus\\Desktop\\projects\\DemoQA\\target\\ValidarMenu.pdf";
 		Document doc = new Document();
 		PdfWriter.getInstance(doc, new FileOutputStream(file));
 		
@@ -23,13 +23,13 @@ public class ITextPdf {
 		doc.add(image);
 		
 		doc.add(new Paragraph ("Ir para o menu:\n"));
-//		Image image2 = Image.getInstance("menu2.png");
-//		image2.scaleToFit(826, 1100);
-//		doc.add(image2);
+		Image image2 = Image.getInstance("menu2.png");
+		image2.scaleToFit(595, 1100);
+		doc.add(image2);
 		
-		doc.add(new Paragraph ("Passar o mouse em main item 2"));
-		doc.add(new Paragraph ("Passar o mouse em sub sub list"));
-		doc.add(new Paragraph ("Acionar sub sub item 2:\n"));
+		doc.add(new Paragraph ("Passar o mouse em main item 2:\n"));
+//		doc.add(new Paragraph ("Passar o mouse em sub sub list"));
+//		doc.add(new Paragraph ("Acionar sub sub item 2:\n"));
 		Image image3 = Image.getInstance("menu3.png");
 		image3.scaleToFit(595, 1100);
 		doc.add(image3);
